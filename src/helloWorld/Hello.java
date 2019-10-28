9847d2044a52ed76bedb63b0cdf3f410c67789e8
Andreapagus <34506169+Andreapagus@users.noreply.github.com>
2019-10-28 18:48:57 +0100

    Update Hello.java

--- src/helloWorld/Hello.java
+++ src/helloWorld/Hello.java
@@ -1,8 +1,11 @@
-package helloWorld;
+package it.helloWorld;
+
+import java.util.logging.*;
 
 public class Hello{
-	public static void main(String args[])
+	public static void main(String[] args)
 	{
+		Logger.getLogger("Hello World!");	
 		System.out.println("Hello world!");
 	}
 }
